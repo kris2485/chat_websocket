556dc085e91fb648094107bfb9426497378fc820
Krzysztof Rajnert <krzysiekrajnert@gmail.com>
2021-11-26 12:48:33 +0100

    Add client content

---
+++ client/app.js
@@ -0,0 +1,49 @@
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+
+let userName = '';
+
+const login = (e) => {
+  e.preventDefault();
+  if (userNameInput.value.length == 0) {
+    alert('Please log in');
+  } else {
+    userName = userNameInput.value;
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
+  }
+};
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if (author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+      <h3 class="message__author">${userName === author ? 'You' : author}</h3>
+      <div class="message__content">
+        ${content}
+      </div>
+    `;
+  messagesList.appendChild(message);
+}
+
+const sendMessage = (e) => {
+  e.preventDefault();
+  if (messageContentInput.value.length == 0) {
+    alert('Type a message');
+  } else {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput.value = '';
+  }
+};
+
+loginForm.addEventListener('submit', (e) => {
+  login(e);
+});
+addMessageForm.addEventListener('submit', (e) => {
+  sendMessage(e);
+});
